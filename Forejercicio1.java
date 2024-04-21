03d083ea3bab618efef1bba6fad6fdad44ae4519
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-04-20 23:19:32 -0600

    Create Forejercicio1.java

---
+++ Forejercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Forejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero, i;
+        System.out.println("Ingresa un número: ");
+        numero = sc.nextInt();
+
+        for (i =1; i<=numero; i ++){
+            System.out.println("Este codigo se imprimira las veces que ingresaste el numero ");
+        }
+            
+    }
+    
+}
